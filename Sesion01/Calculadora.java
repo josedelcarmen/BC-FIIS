088980eb825c5021918d716566bd88049ae37357
josedelcarmen <jose.canchucaja@gmail.com>
2021-09-19 16:05:43 -0500

    Add files via upload

---
+++ Sesion01/Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// Usando una librería de funciones
+		int x = 2, y = 3;
+		int suma = Funciones.sumar(x, y);
+		System.out.println("La suma de " + x + " y de " + y + " es " + suma);	
+	}
+
+}
