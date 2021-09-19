732da9418f8c7aa9e0b1b4cc558a085207e3a928
josedelcarmen <jose.canchucaja@gmail.com>
2021-09-19 15:59:59 -0500

    Add files via upload
    Clase servidora --> API (grupo de clases servidoras --> utilitarios -->paquete)

---
+++ Sesion01/Funciones.java
@@ -0,0 +1,14 @@
+public class Funciones {
+	public static int sumar(int a, int b) {
+		return a + b;
+	}
+	public static int restar(int a, int b) {
+		return a - b;
+	}
+	public static int multiplicar(int a, int b) {
+		return a * b;
+	}
+	public static int dividir(int a, int b) {
+		return a / b;
+	}
+}
