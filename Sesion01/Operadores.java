390c4c6d8177d86683f5d7e501064e39e310becc
josedelcarmen <jose.canchucaja@gmail.com>
2021-09-19 14:54:30 -0500

    Add files via upload

---
+++ Sesion01/Operadores.java
@@ -0,0 +1,14 @@
+public class Operadores {
+
+	public static void main(String[] args) {
+		
+		int a = 4, b = 4;
+		if (a == b) {
+			System.out.println("son iguales"); // digita syso y control + barra espaciadora
+		}
+		else {
+			System.out.println("no son iguales");
+		}
+	}
+
+}
