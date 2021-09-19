7b477022acec6e4e3ab27be6fc5e52d7c5ef9fb7
josedelcarmen <jose.canchucaja@gmail.com>
2021-09-19 16:24:23 -0500

    Update Calculadora.java
    Ejemplo de cómo se usa un método de instancia de otra clase (que e suna librería de funciones)

--- Sesion01/Calculadora.java
+++ Sesion01/Calculadora.java
@@ -5,6 +5,11 @@ public class Calculadora {
 		int x = 2, y = 3;
 		int suma = Funciones.sumar(x, y);
 		System.out.println("La suma de " + x + " y de " + y + " es " + suma);	
+		// Usando un método de instancia ( de objeto)
+		double n1 = 3.14, n2 = 2.38;
+		Funciones objFunciones = new Funciones(); // Creando el objeto (instancia)
+		double sumaDecimales = objFunciones.sumar(n1, n2);
+		System.out.println("La suma de " + n1 + " y de " + n2 + " es " + sumaDecimales);
 	}
 
 }
